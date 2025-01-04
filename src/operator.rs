e7b5f98b6c80ce5e1a959e2ad97b9af42cb53720
oktayudha05 <oktayudha05@gmail.com>
2025-01-04 12:30:15 +0700

    operator

--- src/operator.rs
+++ src/operator.rs
@@ -2,7 +2,17 @@
 pub fn run(){
     let (num1, num2) = (12, 4);
     println!("{}", num1 + num2);
-    
+
     let num3 = num1 * num2;
     println!("num3 {}", num3);
+
+    let perb1 = num1 < num2;
+    println!("perbandingan 1 {}", perb1);
+
+    // negasi operator
+    let (neg1, neg2) = (12, -12);
+    let hasil_negasi1 = -neg1 == neg2;
+    let hasil_negasi2 = !(neg1 == neg2);
+    println!("hasil negasi 1 \t: {hasil_negasi1}");
+    println!("hasil negasi 2 \t: {hasil_negasi2}")
 }
